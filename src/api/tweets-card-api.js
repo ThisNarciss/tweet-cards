12572ac837d5c5af5d623abc98250275c0a956ad
ThisNarciss <tyomaxxx000@gmail.com>
2023-04-21 12:53:44 +0300

    add axios, styled, react router

---
+++ src/api/tweets-card-api.js
@@ -0,0 +1,15 @@
+axios.defaults.baseURL = 'https://6442397876540ce22586f7fa.mockapi.io/api/v1';
+
+export function getUsersData() {
+  return axios
+    .get('/users')
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+}
+
+export function updateUsersData(id) {
+  return axios
+    .put(`/users/:${id}`)
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+}
