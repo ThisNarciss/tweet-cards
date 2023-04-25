f7b06c17d7085fe1f8bd0f459393b90a27a769a2
ThisNarciss <tyomaxxx000@gmail.com>
2023-04-25 11:46:11 +0300

    add vute config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 import css from 'vite-plugin-css';
 
+// console.log(reactRefresh());
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/tweet-cards',
   plugins: [react(), css],
+  resolve: {
+    alias: {
+      // add alias for react-router-dom to resolve properly in your application
+      'react-router-dom': resolve(__dirname, 'node_modules/react-router-dom'),
+    },
+  },
 });
