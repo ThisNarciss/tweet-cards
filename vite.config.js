b42eb0cff906b9609abf3d5e1e0687531144aad3
ThisNarciss <tyomaxxx000@gmail.com>
2023-04-25 12:31:12 +0300

    add 404 code

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import css from 'vite-plugin-css';
 
-// console.log(reactRefresh());
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/tweet-cards/',
   plugins: [react(), css],
